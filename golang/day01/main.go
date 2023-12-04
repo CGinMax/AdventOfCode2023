da72810a40983ad391196efe141a8001d200c393
CGinMax <cginmax1998@gmail.com>
2023-12-04 15:44:07 +0800

    feat: finish day01 all puzzles

--- golang/day01/main.go
+++ golang/day01/main.go
@@ -5,11 +5,79 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func onlyDigital(documents []string) {
+	sum := 0
+	for _, line := range documents {
+		first := 0
+		second := 0
+		for i := 0; i < len(line); i++ {
+			if '0' <= line[i] && line[i] <= '9' {
+				first = int(line[i] - '0')
+				break
+
+			}
+		}
+
+		for i := len(line) - 1; i >= 0; i-- {
+			if '0' <= line[i] && line[i] <= '9' {
+				second = int(line[i] - '0')
+				break
+			}
+		}
+		sum += first*10 + second
+	}
+	fmt.Println("sum:", sum)
+}
+
+func digitalAndLetters(documents []string) {
+
+	letters := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	sum := 0
+	for _, line := range documents {
+		first := 0
+		firstInedx := len(line)
+		second := 0
+		secondIndex := -1
+		for num, numStr := range letters {
+			numIndex := strings.Index(line, strconv.Itoa(num+1))
+			numStrIndex := strings.Index(line, numStr)
+			if numIndex >= 0 && numIndex < firstInedx {
+				first = num + 1
+				firstInedx = numIndex
+			}
+			if numStrIndex >= 0 && numStrIndex < firstInedx {
+				first = num + 1
+				firstInedx = numStrIndex
+			}
+		}
+
+		for num, numStr := range letters {
+			numIndex := strings.LastIndex(line, strconv.Itoa(num+1))
+			numStrIndex := strings.LastIndex(line, numStr)
+			if numIndex >= 0 && numIndex > secondIndex {
+				second = num + 1
+				secondIndex = numIndex
+			}
+			if numStrIndex >= 0 && numStrIndex > secondIndex {
+				second = num + 1
+				secondIndex = numStrIndex
+			}
+		}
+
+		sum += first*10 + second
+	}
+	fmt.Println("sum:", sum)
+}
 
 func main() {
 	var documents []string
 	file, err := os.Open("input")
+	// file, err := os.Open("input_basic")
 	if err != nil {
 		fmt.Println("Error:", err.Error())
 		return
@@ -31,26 +99,7 @@ func main() {
 		documents = append(documents, input)
 	}
 
-	sum := 0
-	for _, line := range documents {
-		first := 0
-		second := 0
-		for i := 0; i < len(line); i++ {
-			if '0' <= line[i] && line[i] <= '9' {
-				first = int(line[i] - '0')
-				break
+	onlyDigital(documents)
+	digitalAndLetters(documents)
 
-			}
-		}
-
-		for i := len(line) - 1; i >= 0; i-- {
-			if '0' <= line[i] && line[i] <= '9' {
-				second = int(line[i] - '0')
-				break
-			}
-		}
-		fmt.Printf("first:%d, second:%d\n", first, second)
-		sum += first*10 + second
-	}
-	fmt.Println("sum is", sum)
 }
